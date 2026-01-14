92f3932df5705ef7a8f08577d025a937ef8b3246
Luano Cesar <luanocesar@gmail.com>
2026-01-14 03:14:41 -0400

    feat: implementing abstract class on child classes and overriding specific method on them

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "980801212", modelo : "NK-001", imei : "8000000001", memoria : 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero : "980811414", modelo : "IP20", imei : "8000000002", memoria : 128);
+iphone.Ligar();
+nokia.InstalarAplicativo("Telegram");
